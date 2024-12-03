d9d07b0cc0682453df354aeeaa307cebed608155
Anel Becirbegovic <anel-b@hotmail.com>
2024-12-03 22:12:11 +0100

    Finished Innovation and Research incl minor changes overall

--- src/navigation.ts
+++ src/navigation.ts
@@ -52,73 +52,11 @@ export const headerData = {
     },
     {
       text: 'Case Studies',
-      links: [
-        {
-          text: 'Blog List',
           href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
     },
     {
       text: 'Contact',
           href: getPermalink('/contact'),
-    },
-    {
-      text: 'Templates',
-      links: [
-        {
-          text: 'About us',
-          href: getPermalink('/templates/about'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ]
     },
   ],
   actions: [{ text: 'Login', href: 'https://setbase.ch', target: '_blank' }],
